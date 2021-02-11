d1f92631ead20a666e2c5f71eb8163158c5e92e2
Ben Lewell <ben@Bens-MacBook-Pro.local>
2021-02-11 11:29:14 +0000

    Updates

---
+++ precache-manifest.70c04660fbfbc9cb15ae14dcaed956f6.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24b86675fafdd30e0a72b504ab1e9722",
+    "url": "/blewell321/material-dashboard.git/index.html"
+  },
+  {
+    "revision": "4b0f6b538629b027d57e",
+    "url": "/blewell321/material-dashboard.git/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "0ff61df98bc775d567f2",
+    "url": "/blewell321/material-dashboard.git/static/css/main.8e9f8467.chunk.css"
+  },
+  {
+    "revision": "4b0f6b538629b027d57e",
+    "url": "/blewell321/material-dashboard.git/static/js/2.107cbd70.chunk.js"
+  },
+  {
+    "revision": "34491c6de1fbcf2388878b87785c74e7",
+    "url": "/blewell321/material-dashboard.git/static/js/2.107cbd70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ff61df98bc775d567f2",
+    "url": "/blewell321/material-dashboard.git/static/js/main.beffea66.chunk.js"
+  },
+  {
+    "revision": "f7bdf0cfc9ea0dd44285692948c2d84f",
+    "url": "/blewell321/material-dashboard.git/static/js/main.beffea66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bc337a0a497b8b1519a",
+    "url": "/blewell321/material-dashboard.git/static/js/runtime-main.31723802.js"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/blewell321/material-dashboard.git/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/blewell321/material-dashboard.git/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/blewell321/material-dashboard.git/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/blewell321/material-dashboard.git/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/blewell321/material-dashboard.git/static/media/sidebar-4.fc9cb053.jpg"
+  }
+]);
